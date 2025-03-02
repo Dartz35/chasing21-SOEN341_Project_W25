73557677fcd474ae5b4995a4fc7eab8d6f34e24f
Christian Jeune <jeuch@outlook.com>
2025-03-02 15:09:36 -0500

    Testing CI-Pipeline

--- test/testCases/loginTest.mjs
+++ test/testCases/loginTest.mjs
@@ -1,11 +1,11 @@
-import { runCSSValidation } from "../test/scripts/styleTest";
+import { runCSSValidation } from "../scripts/styleTest";
 
 // Run tests only in CI/CD
 if (process.env.CI) {
   runCSSValidation(
     "../html/loginPage.html",
-    "../test/expectedResults/expectedloginPageStyles.json",
-    "../test/results/loginPageTestResults.log"
+    "../expectedResults/expectedloginPageStyles.json",
+    "../results/loginPageTestResults.log"
   );
 } else {
   console.log("Running locally: Skipping CSS tests.");
