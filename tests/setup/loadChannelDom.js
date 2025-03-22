d7e11bfca6f616339435fdc606ef99f0926511fc
Christian Jeune <jeuch@outlook.com>
2025-03-22 04:26:10 -0400

    CI test

--- tests/setup/loadChannelDom.js
+++ tests/setup/loadChannelDom.js
@@ -1,4 +1,4 @@
-import { loadDOM } from "../test/setup/loadDom.js";
+import { loadDOM } from "/test/setup/loadDom.js";
 
 loadDOM("channels");
 document.getElementById("channelName").value = "Test Channel";
