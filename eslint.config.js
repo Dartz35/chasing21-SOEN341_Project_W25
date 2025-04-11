6a85f35bc80872227460be43ff69d7d7434c9fb6
Aboubacry Ba <abou9710@gmail.com>
2025-04-10 21:52:52 -0400

    debugging tool file with errors

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import css from "@eslint/css";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+]);
